602a73b651edde277edee9c79fb5cee6ac0b0dec
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-19 20:06:15 +0700

    Add checkbox

---
+++ Checkbox/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="checkbox" name="foods[]" value="Pizza"> Pizza <br>
+        <input type="checkbox" name="foods[]" value="Burger"> Burger <br>
+        <input type="checkbox" name="foods[]" value="Hotdog"> Hotdog <br>
+        <input type="checkbox" name="foods[]" value="Taco"> Taco <br>
+
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
+
+<?php
+
+if (isset($_POST["submit"])) {
+
+    if(empty($_POST["foods"])) {
+        echo "Choose at least one food!"; 
+    } else {
+        foreach($_POST["foods"] as $food) {
+            echo "You choose {$food} <br>";
+        }
+    }
+
+}
