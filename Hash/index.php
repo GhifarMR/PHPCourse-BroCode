35dc2f8014dcd06c57c4f9a17cfa4e58f1bd59dd
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-27 14:51:52 +0700

    Refactor HTML structure and enhance password verification logic

--- Hash/index.php
+++ Hash/index.php
@@ -1,7 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="">Enter Password : </label> <br>
+        <input type="text" name="in_pass">
+        <input type="submit" name="login" value="Login">
+    </form>
+    <br>
+</body>
+</html>
+
+
 <?php
+
 $password = "BurgerDelicious";
 $hash = password_hash($password, PASSWORD_DEFAULT);
 
 echo $hash . "<br>";
+
+if(isset($_POST["login"])) {
+    $input = $_POST["in_pass"];
+
+    if(password_verify($input, $hash)) {
+        echo "Login successfull";
+    } else {
+        echo "Incorrect password";
+    }
+}
 
 ?>
