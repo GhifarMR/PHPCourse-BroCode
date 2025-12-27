84dac66049cb93aa06dd42341d0bbc4c84233ad7
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-12-27 16:30:49 +0700

    Improve error message for database connection and update user registration logic

--- Website/database.php
+++ Website/database.php
@@ -9,7 +9,7 @@ $connection = "";
 try{
     $connection = mysqli_connect($db_server, $db_user, $db_password, $db_name);
 } catch(mysqli_sql_exception) {
-    echo "couldn't connect <br>";
+    echo "couldn't connect to the server <br>";
 }
 
 
--- Website/index.php
+++ Website/index.php
@@ -1,31 +1,52 @@
 <?php
-    /** @var mysqli $connection */
-    include("database.php");
-    
-    $user = "Patrick";
-    $password = "rock3";
-    $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $insert = "INSERT INTO users(user, password)
-            VALUES('$user', '$password_hash')";
-    try{
-        mysqli_query($connection, $insert);
-        echo "user is now registered <br>";
-    } catch(mysqli_sql_exception) {
-        echo "couldn't registered user <br>";
-    }
+/** @var mysqli $connection */
+include("database.php");
 
-    mysqli_close($connection);
+$user = 'OldJenkins';
+$password = 'I like chocolate';
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
+$insert =   "INSERT INTO users (user, password)
+            VALUES ('{$user}', '{$password_hash}')";
+
+try {
+    mysqli_query($connection, $insert);
+    echo "Input successfull <br>";
+} catch (mysqli_sql_exception) {
+    echo "Couldn't input username has been taken <br>";
+}
+
+$select_all =   "SELECT * FROM users";
+                // WHERE user = '$user'";
+
+$result = mysqli_query($connection, $select_all);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<br>" . $row["id"] . "<br>";
+        echo $row["user"] . "<br>";
+        echo $row["reg_date"] . "<br>";
+        echo $row["password"] . "<br>";
+    };
+} else {
+    echo "No user found";
+}
+
+mysqli_close($connection);
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <p>Hello</p>
 </body>
+
 </html>
